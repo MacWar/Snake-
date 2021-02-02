62d8cf2efefcf9ff3d52153d12a3c2530f5801f6
Namtrak <mateosxxx@gmail.com>
2021-02-02 10:39:00 +0100

    nowy

--- snake.js
+++ snake.js
@@ -2,91 +2,91 @@
 // canvas ma 800 x 800 póki co dzielę go na 40 rzędów i 40 lini 
 // czyli jednostką węża jest 20px (np.głowa ma 20x, każda kolejna część ogona bd miała 20px itd) 
 //wczytanie canvasu
-let canv=document.querySelector(".canvas-snake")
-let ctx=canv.getContext('2d');
+let canv = document.querySelector(".canvas-snake")
+let ctx = canv.getContext('2d');
 //tworzenie snaka 
-let snake=[];
-snake[0]={
-    x:0,
-    y:0
+let snake = [];
+snake[0] = {
+    x: 0,
+    y: 0
 }
 //wczytywanie obrazków
-const tlo=new Image();
-tlo.src="img/tlo.jpg";
-let glowa=new Image();
-glowa.src="img/head_lewo.png";
+const tlo = new Image();
+tlo.src = "img/tlo.jpg";
+let glowa = new Image();
+glowa.src = "img/head_lewo.png";
 
 //zmienne
 let kierunek;
 
 //funkcja odpowiadająca za rysowanie na canvasie
-function rysuj(){
-    ctx.drawImage(tlo,0,0);
+function rysuj() {
+    ctx.drawImage(tlo, 0, 0);
     snake.forEach(element => {
-        if(element==snake[0]){
-            ctx.drawImage(glowa,snake[0].x,snake[0].y,20,20);
-            ctx.strokeStyle="black";
-            ctx.strokeRect(element.x,element.y,20,20)
+        if (element == snake[0]) {
+            ctx.drawImage(glowa, snake[0].x, snake[0].y, 20, 20);
+            ctx.strokeStyle = "black";
+            ctx.strokeRect(element.x, element.y, 20, 20)
         }
-        else{
-            ctx.fillStyle="purple";
-            ctx.fillRect(element.x,element.y,20,20)
-            ctx.strokeStyle="black";
-            ctx.strokeRect(element.x,element.y,20,20)
+        else {
+            ctx.fillStyle = "purple";
+            ctx.fillRect(element.x, element.y, 20, 20)
+            ctx.strokeStyle = "black";
+            ctx.strokeRect(element.x, element.y, 20, 20)
         }
     });
 
-    let glowaX=snake[0].x;
-    let glowaY=snake[0].y;
+    let glowaX = snake[0].x;
+    let glowaY = snake[0].y;
 
     //jak was denerwuje że ogon cały czas rośnie to odkomentujcie poniżej xd
     //snake.pop()
 
-    
+
     //ustala nową pozycję
-    switch(kierunek){
+    switch (kierunek) {
         case "lewo":
-        glowaX=glowaX-20;
-        glowa.src="img/head_lewo.png"
-        break;
+            glowaX = glowaX - 20;
+            glowa.src = "img/head_lewo.png"
+            break;
         case "gora":
-        glowaY=glowaY-20;
-        glowa.src="img/head_gora.png"
-        break;
+            glowaY = glowaY - 20;
+            glowa.src = "img/head_gora.png"
+            break;
         case "dol":
-        glowaY=glowaY+20;
-        glowa.src="img/head_dol.png"
-        break;
+            glowaY = glowaY + 20;
+            glowa.src = "img/head_dol.png"
+            break;
         case "prawo":
-        glowaX=glowaX+20;
-        glowa.src="img/head_prawo.png"
-        break;
+            glowaX = glowaX + 20;
+            glowa.src = "img/head_prawo.png"
+            break;
     }
     //aktualizuje snaka
-    snake.unshift({x:glowaX,y:glowaY});
-    
+    snake.unshift({ x: glowaX, y: glowaY });
+
 }
-let odswiez=setInterval(rysuj,150);
+let odswiez = setInterval(rysuj, 150);
 
 //sprawdzanie nacisnietego klawisza
-document.addEventListener("keydown",sprawdzKlaw);
-function sprawdzKlaw(event){
-    switch(event.keyCode){
+document.addEventListener("keydown", sprawdzKlaw);
+function sprawdzKlaw(event) {
+    switch (event.keyCode) {
         case 37:
-            if(kierunek!="prawo")
-            kierunek="lewo";
+            if (kierunek != "prawo")
+                kierunek = "lewo";
             break;
         case 38:
-            if(kierunek!="dol")
-            kierunek="gora";
+            if (kierunek != "dol")
+                kierunek = "gora";
             break;
         case 39:
-            if(kierunek!="lewo")
-            kierunek="prawo";
+            if (kierunek != "lewo")
+                kierunek = "prawo";
             break;
         case 40:
-            if(kierunek!="gora")
-            kierunek="dol";
+            if (kierunek != "gora")
+                kierunek = "dol";
             break;
     }
 }
